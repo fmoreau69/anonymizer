9acc31745689ffeba6f8160202cbaca630372ada
fmoreau <fabien.moreau@ifsttar.fr>
2023-11-09 17:43:15 +0100

    Blur area improved (rectangle + ovoid)

--- anonymize.py
+++ anonymize.py
@@ -1,4 +1,5 @@
 import os
+import re
 import gc
 import cv2
 import torch
@@ -14,6 +15,8 @@ from pathlib import Path
 from .bounds import Bounds
 from ultralytics import YOLO, settings
 from ultralytics.utils import MACOS, WINDOWS
+
+mimetypes.init()
 
 
 class Anonymize:
@@ -31,13 +34,14 @@ class Anonymize:
         os.makedirs(self.source) if not os.path.exists(self.source) else self.source + 'already exists'
         self.destination = './media/output_media'
         os.makedirs(self.destination) if not os.path.exists(self.destination) else self.destination + 'already exists'
-        self.input_path = None
+        self.input_path = None  # "D:\LESCOT\INFORMATIQUE\DEVELOPPEMENT\DEEP_LEARNING\MEDIA_SOURCE\Img\Faces_01.jpg"
         self.output_path = None
         self.save_path = './runs'
         self.models_dir = './models'
         settings.update({'runs_dir': self.save_path, 'weights_dir': self.models_dir})
 
         # Model settings
+        self.class_list = []
         self.classes2blur = ['face', 'plate']
         # self.classes2blur = ['person', 'car', 'truck', 'bus']
         self.model_name = None
@@ -55,7 +59,7 @@ class Anonymize:
         self.plotted_img = None
 
         # Option settings
-        self.blur_ratio = 20
+        self.blur_ratio = 25
         self.rounded_edges = 5
         self.ROI_enlargement = 1.05
         self.conf = 0.25
@@ -72,41 +76,48 @@ class Anonymize:
         self.model_name = 'yolov8n-seg.pt' if self.task == 'segment' else "yolov8n.pt"
         if any([classe in self.classes2blur for classe in ['face', 'plate']]):
             self.model_name = "yolov8m_faces&plates_720p.pt"  # "yolov8m_faces&plates_1080p.pt"
-        self.model_path = kwargs['model_path'] if 'model_path' in kwargs else os.path.join(self.models_dir,
-                                                                                           self.model_name)
+        self.model_path = kwargs['model_path'] if 'model_path' in kwargs \
+            else os.path.join(self.models_dir, self.model_name)
         print('Model used: ' + str(self.model_path))
         self.model = YOLO(self.model_path)
+        self.class_list = self.model.model.names.values()
 
     def process(self, **kwargs):
         if self.model is not None:
-            if os.path.isdir(kwargs['media_path'] if 'media_path' in kwargs else self.source):
-                for media in os.listdir(self.source):
-                    self.input_path = os.path.join(self.source, media)
+            self.input_path = self.source if self.input_path is None else self.input_path
+            self.input_path = kwargs['media_path'] if 'media_path' in kwargs else self.input_path
+            if os.path.isdir(self.input_path):
+                for media in os.listdir(self.input_path):
+                    self.input_path = os.path.join(self.input_path, media)
                     self.output_path = os.path.join(self.destination, media[:-4] + '_blurred' + media[-4:])
                     Anonymize.setup_source(self)
                     Anonymize.apply_process(self, **kwargs)
             else:
-                media_path = kwargs['media_path'] if 'media_path' in kwargs else self.source
-                self.input_path = media_path
-                self.output_path = media_path[:-4].replace('input', 'output') + '_blurred' + media_path[-4:]
-                Anonymize.setup_source(self)
+                self.output_path = self.input_path[:-4].replace('input', 'output') + '_blurred' + self.input_path[-4:]
+                Anonymize.setup_source(self, **kwargs)
                 Anonymize.apply_process(self, **kwargs)
         else:
             print('No model is loaded')
 
-    def setup_source(self):
+    def setup_source(self, **kwargs):
         print('Setting up media: ' + str(self.input_path))
         with imageio.get_reader(self.input_path) as reader:
             self.meta_data = reader.get_meta_data()
             if 'fps' in self.meta_data:
+                media_ext = (kwargs['media_ext'] if 'media_ext' in kwargs else '.mp4')
                 suffix, fourcc = ('.mp4', 'avc1') if MACOS else ('.avi', 'WMV2') if WINDOWS else ('.avi', 'MJPG')
                 save_path = str(Path(self.output_path).with_suffix(suffix))
                 self.vid_writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*fourcc), self.meta_data['fps'],
                                                   (self.meta_data['size'][0], self.meta_data['size'][1]))
-                # self.vid_writer = dict.fromkeys(range(int(self.meta_data['duration'] * self.meta_data['fps'])))
 
     def apply_process(self, **kwargs):
         # Apply model
+        self.classes2blur = kwargs['classes2blur'] if 'classes2blur' in kwargs else self.classes2blur
+        classes2blur_by_names = re.findall(r"\'(.*?)\'", str(self.classes2blur))
+        classes2blur_by_index = []
+        for idx, elem in enumerate(self.class_list):
+            if elem in classes2blur_by_names:
+                classes2blur_by_index.append(idx)
         self.results = self.model.track(
             source=(kwargs['media_path'] if 'media_path' in kwargs else self.input_path),
             # stream=True,
@@ -117,7 +128,7 @@ class Anonymize:
             imgsz=self.meta_data['size'][0] if 'size' in self.meta_data else self.meta_data['shape'][0],
             save=self.save,
             save_txt=self.save_txt,
-            # classes=self.classes2blur,
+            classes=classes2blur_by_index,
             conf=(kwargs['detection_threshold'] if 'detection_threshold' in kwargs else self.conf),
             show=(kwargs['show_preview'] if 'show_preview' in kwargs else self.show),
             boxes=(kwargs['show_boxes'] if 'show_boxes' in kwargs else self.boxes),
@@ -138,7 +149,7 @@ class Anonymize:
     def blur_results(self, **kwargs):
         plot_args = {'line_width': None, 'boxes': False, 'conf': False, 'labels': False}
         classes2blur = (kwargs['classes2blur'] if 'classes2blur' in kwargs else self.classes2blur)
-        blur_ratio = int(kwargs['blur_ratio'] * 100 if 'blur_ratio' in kwargs else self.blur_ratio)
+        blur_ratio = int(kwargs['blur_ratio'] if 'blur_ratio' in kwargs else self.blur_ratio)
         rounded_edges = int(kwargs['rounded_edges'] if 'rounded_edges' in kwargs else self.rounded_edges)
         roi_enlargement = (kwargs['ROI_enlargement'] if 'ROI_enlargement' in kwargs else self.ROI_enlargement)
         detection_threshold = (kwargs['detection_threshold'] if 'detection_threshold' in kwargs else self.conf)
@@ -148,26 +159,22 @@ class Anonymize:
                 for d in result.boxes:  # Loop on boxes
                     label = result.names[int(d.cls)]
                     if label in classes2blur and float(d.conf) >= detection_threshold:
-                        bounds = (Bounds(int(d.xyxy[0][0]), int(d.xyxy[0][1]), int(d.xyxy[0][2]), int(d.xyxy[0][3])).
-                                  scale(im0.shape, roi_enlargement).expand(im0.shape, rounded_edges))
-                        crop_obj = im0[bounds.y_min:bounds.y_max, bounds.x_min:bounds.x_max]
-                        blur_area = np.full((bounds.y_max - bounds.y_min, bounds.x_max - bounds.x_min, 3), 0,
-                                            dtype=np.float64)
-                        # blur_area = crop_obj
+                        x, y = int(d.xyxy[0][0]), int(d.xyxy[0][1])
+                        w, h = int(d.xyxy[0][2]) - x, int(d.xyxy[0][3]) - y
+                        bounds = Bounds(x, y, x + w, y + h).scale(im0.shape, roi_enlargement).expand(im0.shape, rounded_edges)
+                        x, y, w, h = bounds.x_min, bounds.y_min, bounds.x_max - bounds.x_min, bounds.y_max - bounds.y_min
+                        blur_area = im0[y:y + h, x:x + w]
                         if label == 'face' or label == 'person':
-                            center, axes = bounds.ellipse_coordinates()
-                            blur_area = cv2.ellipse(blur_area, center, axes, 0, 0, 360, color=[1, 1, 1], thickness=-1)
+                            mask = np.zeros((h, w), dtype=np.uint8)
+                            center, axes = (w // 2, h // 2), (w // 2, h // 2)
+                            cv2.ellipse(mask, center, axes, 0, 0, 360, (255, 255, 255), thickness=-1)
+                            im0_cropped = cv2.bitwise_and(blur_area, blur_area, mask=cv2.bitwise_not(mask))
+                            blurred_area = cv2.GaussianBlur(blur_area, (blur_ratio, blur_ratio), 0, dst=mask)
+                            blurred_area = cv2.addWeighted(im0_cropped, 0.5, blurred_area, 0.5, 0)
                         else:
-                            blur_area = cv2.rectangle(blur_area, bounds.pt1(), bounds.pt2(), color=[1, 1, 1],
-                                                      thickness=-1)
-                        if rounded_edges > 0:
-                            blur_area = cv2.blur(blur_area, (rounded_edges, rounded_edges), 0)
-                        clear_area = np.full((bounds.y_max - bounds.y_min, bounds.x_max - bounds.x_min, 3), 1,
-                                             dtype=np.float64) - blur_area
-                        blurred_box = cv2.blur(crop_obj, (blur_ratio, blur_ratio), 0)
-                        im0[bounds.y_min:bounds.y_max,
-                        bounds.x_min:bounds.x_max] = blurred_box  # clear_area * im0 + blur_area * blurred_box
-            self.plotted_img = im0.astype(np.uint8)
+                            blurred_area = cv2.GaussianBlur(blur_area, (blur_ratio, blur_ratio), 0)
+                        im0[y:y + h, x:x + w] = blurred_area
+                    self.plotted_img = im0
             Anonymize.write_media(self)
 
     def write_media(self):
